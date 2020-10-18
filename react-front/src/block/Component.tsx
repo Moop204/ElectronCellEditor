14e8dc464109fcee239672e8a47692116ce55e84
Moop204 <andrew.nguyen2045@outlook.com>
2020-10-18 16:12:55 +1100

    Conencted to electron app

--- react-front/src/block/Component.tsx
+++ react-front/src/block/Component.tsx
@@ -1,43 +1,46 @@
-import { useStyles } from './Block'
-import React from 'react'
-import { Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Typography from '@material-ui/core/Typography';
-import AddIcon from '@material-ui/icons/Add';
-import LabelIcon from '@material-ui/icons/Label';
-import TextField from '@material-ui/core/TextField';
+import { useStyles } from "./Block";
+import React from "react";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Typography from "@material-ui/core/Typography";
+import AddIcon from "@material-ui/icons/Add";
+import LabelIcon from "@material-ui/icons/Label";
+import TextField from "@material-ui/core/TextField";
 
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 
 const Layout = () => {
-  return ( 
+  return (
     <Grid container spacing={3}>
-      < Grid item xs={12}> 
+      <Grid item xs={12}>
         <TextField
           required
           id="filled-required"
           label="Name"
           variant="filled"
         />
-      </Grid> 
-      < Grid item xs={12}> 
-        <SampleComponentBlock/>
-      </Grid> 
-      < Grid item xs={12}> 
-        <NewComponentBlock/>
-      </Grid> 
-    </Grid> 
+      </Grid>
+      <Grid item xs={12}>
+        <NewComponentBlock />
+      </Grid>
+    </Grid>
   );
-}
+};
 
-const NewComponentBlock = () => { 
+const NewComponentBlock = () => {
   const style = useStyles();
   return (
     <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-      > 
-        <Typography className={style.heading}><AddIcon/><LabelIcon/>Component</Typography>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={style.heading}>
+          <AddIcon />
+          <LabelIcon />
+          Component
+        </Typography>
       </AccordionSummary>
       <AccordionDetails>
         <TextField
@@ -46,62 +49,26 @@ const NewComponentBlock = () => {
           label="Name"
           variant="filled"
         />
-      </AccordionDetails> 
-
+      </AccordionDetails>
     </Accordion>
-  )  
-}
+  );
+};
 
 const ComponentBlock = () => {
   const style = useStyles();
   return (
     <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-      > 
-        <Typography className={style.heading}><LabelIcon/>Component</Typography>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography className={style.heading}>
+          <LabelIcon />
+          Component
+        </Typography>
       </AccordionSummary>
       <AccordionDetails>
         <Layout />
-      </AccordionDetails> 
-
+      </AccordionDetails>
     </Accordion>
-  )  
-}
-
-const SampleComponentBlock = () => { 
-  const style = useStyles();
-  return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-      > 
-        <Typography className={style.heading}><LabelIcon/>Component</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <SampleLayout />
-      </AccordionDetails> 
-
-    </Accordion>
-  )  
-}
-
-const SampleLayout = () => {
-  return ( 
-    <Grid container spacing={3}>
-      < Grid item xs={12}> 
-        <TextField
-          required
-          id="filled-required"
-          label="Name"
-          variant="filled"
-        />
-      </Grid> 
-      < Grid item xs={12}> 
-        <NewComponentBlock/>
-      </Grid> 
-    </Grid> 
   );
-}
+};
 
-export { SampleComponentBlock, ComponentBlock, NewComponentBlock }
+export { ComponentBlock, NewComponentBlock };
--- react-front/src/dashboard/Dashboard.tsx
+++ react-front/src/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Terminal } from "./Terminal";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
@@ -6,9 +7,7 @@ import { NewModel, Model } from "../block/Model";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-
 import Box from "@material-ui/core/Box";
-import { Button, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,6 +16,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     paper: {
       padding: theme.spacing(2),
+      textAlign: "center",
       color: theme.palette.text.secondary,
     },
   })
@@ -49,42 +49,13 @@ const Editor = () => {
   );
 };
 
-const Terminal = () => {
-  const classes = useStyles();
-  return (
-    <Box height="25%">
-      <AppBar position="static">
-        <Tabs aria-label="simple tabs example">
-          <Tab label="Terminal" />
-        </Tabs>
-      </AppBar>
-      <Paper variant="outlined" square>
-        Error (errno, lineno): Component test_component_name is not correctly
-        formed
-      </Paper>
-    </Box>
-  );
-};
-
-const View = () => {
-  const classes = useStyles();
-  return (
-    <Box height="50%">
-      <Paper className={classes.paper}>
-        <Typography>View</Typography>
-        <Button>View</Button>
-      </Paper>
-    </Box>
-  );
-};
-
 const Dashboard = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid container item xs={3}>
+        <Grid container item xs={4}>
           <Box height="50%">
             <Paper className={classes.paper}>
               <Paper className={classes.paper}>
@@ -95,9 +66,6 @@ const Dashboard = () => {
               </Paper>
             </Paper>
           </Box>
-        </Grid>
-        <Grid item xs={1}>
-          <Terminal />
         </Grid>
         <Grid item xs={8}>
           <Editor />
