c3d52fa06a69886194ce4113b0e34ef2e5cacdeb
Moop204 <andrew.nguyen2045@outlook.com>
2021-03-14 20:19:54 +1100

    Tests for first iteration

--- src/AsyncMain.tsx
+++ src/AsyncMain.tsx
@@ -1,13 +1,23 @@
+import { Parser } from 'webpack';
 import libcellModule from './wasm/libcellml';
 const fs = require('fs');
 const { ipcMain } = require('electron');
 
+interface ImportInterface {
+  model: string;
+  valid: boolean;
+}
+
+const importFile = (fileLoc: string, parser) => {
+  const file: string = fs.readFileSync(fileLoc, 'utf8');
+  //    const loadfile: string = fs.readFileSync(tmpArg, 'utf8');
+  const model: string = parser.parseModel(file);
+  const valid = true;
+  return { model, valid };
+};
+
 const mainAsync = async () => {
   const libcellml = await libcellModule();
-  const file: string = fs.readFileSync(
-    'src/example/complex_encapsulation.xml',
-    'utf8'
-  );
   const parser = new libcellml.Parser();
   const printer = new libcellml.Printer();
   /*
@@ -19,12 +29,15 @@ const mainAsync = async () => {
   // Waits for invocation from renderer and load in the requested file
   ipcMain.handle('loadFile', async (event: Event, arg: string) => {
     console.log('Requested we open ' + arg);
-    const tmpArg = 'src/example/complex_encapsulation.xml'; // To be properly implemented
-    const loadfile: string = fs.readFileSync(tmpArg, 'utf8');
-    const readModel = parser.parseModel(file);
-    const printedModel = printer.printModel(readModel);
+    // const tmpArg = 'src/example/complex_encapsulation.xml'; // To be properly implemented
+    const { model, valid } = importFile(
+      'src/example/complex_encapsulation.xml',
+      parser
+    );
+    const printedModel = printer.printModel(model);
+    console.log(printedModel);
     return printedModel;
   });
 };
 
-export { mainAsync };
+export { mainAsync, importFile };
