85458ab27e2ba6db86000dea3508ed47175f80a5
Moop204 <andrew.nguyen2045@outlook.com>
2021-07-10 03:38:34 +1000

    More natural save system

--- src/utility/Save.ts
+++ src/utility/Save.ts
@@ -1,25 +1,34 @@
 import { BrowserWindow } from "electron";
+import { flip } from "lodash";
 const { dialog } = require("electron");
 const fs = require("fs");
 
-const SaveAs = (content: string) => {
+const SaveAs = async (content: string, recentFile: string) => {
   const options = {
     title: "Save file",
     buttonLabel: "Save",
-
     filters: [{ name: "All Files", extensions: ["*"] }],
   };
 
   const mainWindow = BrowserWindow.getFocusedWindow();
 
-  dialog
-    .showSaveDialog(mainWindow, options)
-    .then(({ filePath }) => {
-      fs.writeFileSync(filePath, content, "utf-8");
-    })
-    .catch((e) => {
-      console.log("Failed to save");
-    });
+  if (recentFile !== "") {
+    fs.writeFileSync(recentFile, content, "utf-8");
+    return recentFile;
+  } else {
+    let file = "";
+    const res = await dialog
+      .showSaveDialog(mainWindow, options)
+      .then(({ filePath }) => {
+        fs.writeFileSync(filePath, content, "utf-8");
+        return filePath;
+      })
+      .catch((e) => {
+        console.log("Failed to save new file");
+        return recentFile;
+      });
+    return res;
+  }
 };
 
 export { SaveAs };
