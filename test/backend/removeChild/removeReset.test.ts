9628d796eac96d7708efbc51bd9fdd148f415857
Moop204 <andrew.nguyen2045@outlook.com>
2021-07-28 00:32:29 +1000

    unit test for removing Reset

--- test/backend/removeChild/removeReset.test.ts
+++ test/backend/removeChild/removeReset.test.ts
@@ -4,16 +4,18 @@ import {
   Model,
   Parser,
   Printer,
+  Reset,
   Variable,
 } from "../../../src/types/ILibcellml";
 import FileManagement from "../../../src/backend/FileManagement";
 import assert from "assert";
 import { IChild } from "../../../src/types/IProperties";
 import { removeVariable } from "../../../src/backend/removeChild/removeVariable";
+import { removeReset } from "../../../src/backend/removeChild/removeReset";
 
-describe("Removing CellML Variable into property format", function () {
+describe("Removing CellML Reset into property format", function () {
   this.timeout(5000);
-  it("Removing Variable", async () => {
+  it("Removing Reset", async () => {
     const fm = new FileManagement();
     await fm.init();
     const m: Model = new fm._cellml.Model();
@@ -23,7 +25,16 @@ describe("Removing CellML Variable into property format", function () {
     const v: Variable = new fm._cellml.Variable();
     v.setName("v1");
     v.setUnitsByName("kelvin");
+    const r: Reset = new fm._cellml.Reset();
+    r.setOrder(1);
+    const mathValue =
+      `<math xmlns="http://www.w3.org/1998/Math/MathML"><ci>v1</ci></math>`.trim();
+    r.setResetValue(mathValue);
+    r.setTestValue(mathValue);
+    r.setVariable(v);
+    r.setTestVariable(v);
     component.addVariable(v);
+    component.addReset(r);
     m.addComponent(component);
 
     const printer: Printer = new fm._cellml.Printer();
@@ -35,14 +46,14 @@ describe("Removing CellML Variable into property format", function () {
       index: 0,
     };
 
-    removeVariable(fm, child);
+    removeReset(fm, child);
 
     const postRemoval = fm.getContent();
     const parser: Parser = new fm._cellml.Parser();
     const postModel = parser.parseModel(postRemoval);
     // Check attributes of element are preserved
     assert.strictEqual(
-      postModel.componentByIndex(0).variableCount(),
+      postModel.componentByIndex(0).resetCount(),
       0,
       "No variables remain after removing only variable."
     );
