f14c12943950fd0da783c91776efafc50a4b057d
Moop204 <andrew.nguyen2045@outlook.com>
2021-08-01 05:03:57 +1000

    Use single parser instead of regenerating it

--- src/backend/utility/getAllUnitsNames.ts
+++ src/backend/utility/getAllUnitsNames.ts
@@ -1,13 +1,10 @@
-import { Parser } from "../../types/ILibcellml";
 import FileManagement from "../FileManagement";
 
 const getAllUnitsNames = async (fm: FileManagement) => {
   if (!fm._cellmlLoaded) {
     await fm.init();
   }
-  const libcellml = fm._cellml;
-  const parser: Parser = new libcellml.Parser();
-  const model = parser.parseModel(fm.content);
+  const model = fm._parser.parseModel(fm.content);
   const res = [];
   const unitsCount = model.unitsCount();
   for (let i = 0; i < unitsCount; i++) {
